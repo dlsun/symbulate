7bd989f1a4d715c978b6379d1d87209d4c99978f
Dennis Sun <dlsun@pcp077695pcs.wireless.calpoly.edu>
2016-09-22 14:37:37 -0700

    removed dependency on Numpy, using tuples everywhere

--- symbulate/random_variables.py
+++ symbulate/random_variables.py
@@ -1,4 +1,3 @@
-import numpy as np
 import matplotlib.pyplot as plt
 
 from numbers import Number
@@ -6,18 +5,23 @@ from copy import deepcopy as copy
 
 from .results import RVResults
 
+def is_scalar(x):
+    if isinstance(x, Number):
+        return True
+    else:
+        return False
+
+def is_vector(x):
+    if hasattr(x, "__iter__") and all(is_scalar(i) for i in x):
+        return True
+    else:
+        return False
+
 class RV:
 
     def __init__(self, probSpace, fun=lambda x: x):
         self.probSpace = probSpace
-        # convert vector outputs to Numpy arrays
-        # (to support vector addition, etc.)
-        def fun1(outcome):
-            out = fun(outcome)
-            if hasattr(out, "__len__"):
-                out = np.array(out)
-            return out
-        self.fun = fun1
+        self.fun = fun
 
     def draw(self):
         return self.fun(self.probSpace.draw())
@@ -38,76 +42,125 @@ class RV:
         return self.apply(lambda x: x[i])
 
     def __add__(self, other):
-        if isinstance(other, Number):
+        if is_scalar(other):
             return self.apply(lambda x: x + other)
         self.check_same_probSpace(other)
         def fun(outcome):
-            return self.fun(outcome) + other.fun(outcome)
-        return RV(self.probSpace, copy(fun))
+            a = self.fun(outcome)
+            b = other.fun(outcome)
+            if is_vector(a) and is_vector(b) and len(a) == len(b):
+                return tuple(i + j for i, j in zip(a, b))
+            elif is_scalar(a) and is_scalar(b):
+                return a + b
+            else:
+                raise Exception("I don't know how to add those two random variables.")
+        return RV(self.probSpace, fun)
 
     def __radd__(self, other):
         return self.__add__(other)
 
     def __sub__(self, other):
-        if isinstance(other, Number):
+        if is_scalar(other):
             return self.apply(lambda x: x - other)
         self.check_same_probSpace(other)
         def fun(outcome):
-            return self.fun(outcome) - other.fun(outcome)
+            a = self.fun(outcome)
+            b = other.fun(outcome)
+            if is_vector(a) and is_vector(b) and len(a) == len(b):
+                return tuple(i - j for i, j in zip(a, b))
+            elif is_scalar(a) and is_scalar(b):
+                return a - b
+            else:
+                raise Exception("I don't know how to subtract those two random variables.")
         return RV(self.probSpace, fun)
 
-    def __sub__(self, other):
+    def __rsub__(self, other):
         return -1 * self.__sub__(other)
 
     def __mul__(self, other):
-        if isinstance(other, Number):
+        if is_scalar(other):
             return self.apply(lambda x: x * other)
         self.check_same_probSpace(other)
         def fun(outcome):
-            return self.fun(outcome) * other.fun(outcome)
+            a = self.fun(outcome)
+            b = other.fun(outcome)
+            if is_vector(a) and is_vector(b) and len(a) == len(b):
+                return tuple(i * j for i, j in zip(a, b))
+            elif is_scalar(a) and is_scalar(b):
+                return a * b
+            else:
+                raise Exception("I don't know how to multiply those two random variables.")
         return RV(self.probSpace, fun)
 
     def __rmul__(self, other):
         return self.__mul__(other)
 
     def __truediv__(self, other):
-        if isinstance(other, Number):
+        if is_scalar(other):
             return self.apply(lambda x: x / other)
         self.check_same_probSpace(other)
         def fun(outcome):
-            return self.fun(outcome) / other.fun(outcome)
+            a = self.fun(outcome)
+            b = other.fun(outcome)
+            if is_vector(a) and is_vector(b) and len(a) == len(b):
+                return tuple(i / j for i, j in zip(a, b))
+            elif is_scalar(a) and is_scalar(b):
+                return a / b
+            else:
+                raise Exception("I don't know how to divide those two random variables.")
         return RV(self.probSpace, fun)
 
     def __rtruediv__(self, other):
-        if isinstance(other, Number):
+        if is_scalar(other):
             return self.apply(lambda x: other / x)
         self.check_same_probSpace(other)
         def fun(outcome):
-            return other.fun(outcome) / self.fun(outcome)
+            a = self.fun(outcome)
+            b = other.fun(outcome)
+            if is_vector(a) and is_vector(b) and len(a) == len(b):
+                return tuple(j / i for i, j in zip(a, b))
+            elif is_scalar(a) and is_scalar(b):
+                return b / a
+            else:
+                raise Exception("I don't know how to divide those two random variables.")
         return RV(self.probSpace, fun)
 
     def __pow__(self, other):
-        if isinstance(other, Number):
+        if is_scalar(other):
             return self.apply(lambda x: x ** other)
         self.check_same_probSpace(other)
         def fun(outcome):
-            return self.fun(outcome) ** other.fun(outcome)
+            a = self.fun(outcome)
+            b = other.fun(outcome)
+            if is_vector(a) and is_vector(b) and len(a) == len(b):
+                return tuple(i ** j for i, j in zip(a, b))
+            elif is_scalar(a) and is_scalar(b):
+                return a ** b
+            else:
+                raise Exception("I don't know how to raise that random variable to that power.")
         return RV(self.probSpace, fun)
 
     def __rpow__(self, other):
-        if isinstance(other, Number):
+        if is_scalar(other):
             return self.apply(lambda x: other ** x)
         self.check_same_probSpace(other)
         def fun(outcome):
-            return other.fun(outcome) ** self.fun(outcome)
+            a = self.fun(outcome)
+            b = other.fun(outcome)
+            if is_vector(a) and is_vector(b) and len(a) == len(b):
+                return tuple(j ** i for i, j in zip(a, b))
+            elif is_scalar(a) and is_scalar(b):
+                return b ** a
+            else:
+                raise Exception("I don't know how to raise that random variable to that power.")
         return RV(self.probSpace, fun)
                 
     def __and__(self, other):
         self.check_same_probSpace(other)
         def fun(outcome):
             a = self.fun(outcome)
-            a = tuple(a) if hasattr(a, "__len__") else (a, )
             b = other.fun(outcome)
-            b = tuple(b) if hasattr(b, "__len__") else (b, )
+            a = tuple(a) if is_vector(a) else (a, )
+            b = tuple(b) if is_vector(b) else (b, )
             return a + b
         return RV(self.probSpace, fun)
