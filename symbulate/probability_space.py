42007bb58dcd76ad3dd44fb777b9b0d85f2e0a73
Dennis Sun <dlsun@Denniss-MacBook-Pro.local>
2016-09-27 23:20:12 -0700

    adding documentation, adding order matters to BoxModel

--- symbulate/probability_space.py
+++ symbulate/probability_space.py
@@ -3,14 +3,26 @@ import numpy as np
 from .results import Results
 
 class ProbabilitySpace:
+    """Defines a probability space
 
-    results = Results
+    Attributes:
+      draw (function): A function explaining how to draw one 
+        outcome from the probability space.
+    """
 
     def __init__(self, draw):
         self.draw = draw
 
     def sim(self, n):
-        return self.results(self.draw() for _ in range(n))
+        """Simulate n draws from probability space.
+
+        Args:
+          n (int): How many draws to make.
+
+        Returns:
+          Results: A list-like object containing the simulation results.
+        """
+        return Results(self.draw() for _ in range(n))
 
     def __mul__(self, other):
         def draw():
@@ -25,21 +37,44 @@ class ProbabilitySpace:
         return ProbabilitySpace(draw)
 
 class BoxModel(ProbabilitySpace):
+    """Defines a probability space from a box model.
 
-    def __init__(self, box, size=None, replace=True, probs=None):
+    Attributes:
+      box (list): The box to sample from.
+      size (int): How many draws to make.
+      replace (bool): Sample with replacement or without?
+      probs (list): Probabilities of sampling each ticket
+        (by default, all tickets are equally likely)
+      order_matters (bool): Should we count different
+        orderings of the same tickets as different outcomes?
+        Essentially, this determines whether the draws are
+        sorted before returning or not.
+    """
+
+    def __init__(self, box, size=None, replace=True, probs=None, order_matters=True):
         self.box = box
         self.size = None if size == 1 else size
         self.replace = replace
         self.probs = probs
+        self.order_matters = order_matters
 
     def draw(self):
         inds = np.random.choice(len(self.box), self.size, self.replace, self.probs)
         if self.size is None:
             return self.box[inds]
         else:
-            return tuple(self.box[i] for i in inds)
+            draws = [self.box[i] for i in inds]
+            if not self.order_matters:
+                draws.sort()
+            return tuple(draws)
 
 class DeckOfCards(BoxModel):
+    """Defines the probability space for drawing from a deck of cards.
+
+    Attributes:
+      size (int): How many draws to make.
+      replace (bool): Sample with replacement or without?
+    """
     
     def __init__(self, size=None, replace=False):
         self.box = []
