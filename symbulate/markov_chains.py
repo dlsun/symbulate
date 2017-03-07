94b658e925d92459ce9eab220fe1cc02523bcafc
Dennis Sun <dsun09@calpoly.edu>
2017-03-07 12:47:06 -0800

    changing implementation of CTMC

--- symbulate/markov_chains.py
+++ symbulate/markov_chains.py
@@ -34,12 +34,8 @@ class MarkovChain(RandomProcess):
         
 class ContinuousTimeMarkovChain(RandomProcess):
 
-    def __init__(self, generator_matrix, initial_dist, state_labels=None):
-        m = len(initial_dist)
-        T = TimeIndex(fs=float("inf"))
-
-        # check that generator_matrix is valid
-        for i, row in enumerate(generator_matrix):
+    def check_generator(self):
+        for i, row in enumerate(self.generator_matrix):
             if sum(row) != 0:
                 raise Exception("Rows of a generator matrix must sum to 0.")
             for j, q in enumerate(row):
@@ -51,52 +47,59 @@ class ContinuousTimeMarkovChain(RandomProcess):
                     if row[j] < 0:
                         raise Exception("Off-diagonal elements of a generator matrix " +
                                         "cannot be negative.")
+    
+    def __init__(self, generator_matrix, initial_dist, state_labels=None):
+        m = len(initial_dist)
+        T = TimeIndex(fs=float("inf"))
 
-        def draw():
-            seed = get_seed()
-            
-            def states(t):
-                np.random.seed(seed)
-                state = np.random.choice(range(m), p=initial_dist)
-                total_time = 0
-                while True:
-                    row = generator_matrix[state]
-                    rate = -row[state]
-                    total_time += Exponential(rate).draw()
-                    if total_time > t:
-                        break
-                    probs = [p / rate if p >= 0 else 0 for p in row]
-                    state = np.random.choice(range(m), p=probs)
-                if state_labels is None:
-                    return state
-                else:
-                    return state_labels[state]
-                
-            def jump_times(n):
-                np.random.seed(seed)
-                state = np.random.choice(range(m), p=initial_dist)
-                total_time = 0
-                for _ in range(int(n)):
-                    row = generator_matrix[state]
-                    rate = -row[state]
-                    total_time += Exponential(rate).draw()
-                    probs = [p / rate if p >= 0 else 0 for p in row]
-                    state = np.random.choice(range(m), p=probs)
-                return total_time
-                
-            return InfiniteSequence(states, T), InfiniteSequence(jump_times, TimeIndex(1))
+        self.generator_matrix = generator_matrix
+        self.initial_dist = initial_dist
+        self.state_labels = state_labels
+
+        # check that generator matrix is valid
+        self.check_generator()
+
+        # determine transition matrix
+        transition_matrix = []
+        for i, row in enumerate(generator_matrix):
+            rate = -row[i]
+            transition_matrix.append(
+                [p / rate if j != i else 0 for j, p in enumerate(row)]
+            )
+        self.transition_matrix = transition_matrix
+
+        # probability space for the states
+        P_states = MarkovChain(transition_matrix, initial_dist).probSpace
+
+        # probability space for the jump times
+        P_times = Exponential(1) ** float("inf")
 
         def fun(x, t):
-            return x[0][t]
+            states, times = x[0], x[1]
 
-        super().__init__(ProbabilitySpace(draw), T, fun)
+            total_time = 0
+            n = 0
+            while True:
+                state = states[n]
+                rate = -generator_matrix[state][state]
+                total_time += times[n] / rate
+                if total_time > t:
+                    break
+                n += 1
+            
+            return state
+
+        super().__init__(P_states * P_times, T, fun)
         
     def JumpTimes(self):
         def fun(x, n):
-            return x[1][n]
+            total_time = 0
+            for i in range(n):
+                total_time += x[1][i]
+            return total_time
         return RandomProcess(self.probSpace, TimeIndex(1), fun)
 
     def InterjumpTimes(self):
         def fun(x, n):
-            return x[1][n + 1] - x[1][n]
+            return x[1][n]
         return RandomProcess(self.probSpace, TimeIndex(1), fun)
