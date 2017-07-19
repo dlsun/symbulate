e8957dbcb40cb1633c61cb50ce5ea8529e4a4e32
Dennis Sun <dlsun@Denniss-MBP.lan>
2017-07-18 23:49:26 -0700

    adding a few more tests

--- symbulate/tests/test_distributions.py
+++ symbulate/tests/test_distributions.py
@@ -1,16 +1,34 @@
 import unittest
+import numpy as np
 import scipy.stats as stats
 
 from symbulate import *
 
 Nsim = 10000
 
+
 class TestBernoulli(unittest.TestCase):
 
-    def test_bernolli_sum(self):
+    def test_p_one(self):
+        X = RV(Bernoulli(1))
+        sims = X.sim(Nsim)
+        self.assertTrue(all(sim == 1 for sim in sims))
+    
+    def test_sum(self):
         X = RV(Bernoulli(.4) ** 5)
         sims = X.apply(sum).sim(Nsim)
         obs = list(sims.tabulate().values())
-        exp = Nsim * stats.binom.pmf(range(6), n=5, p=.4)
+        exp = Nsim * stats.binom(n=5, p=.4).pmf(range(6))
         pval = stats.chisquare(obs, exp).pvalue
-        self.assertTrue(pval > .001)
+        self.assertTrue(pval > .01)
+
+
+class TestNormal(unittest.TestCase):
+
+    def test_sum(self):
+        X = RV(Normal(mean=-1, sd=2) ** 3)
+        sims = X.apply(sum).sim(Nsim)
+        cdf = stats.norm(loc=-3,
+                         scale=np.sqrt(12)).cdf
+        pval = stats.kstest(sims, cdf).pvalue
+        self.assertTrue(pval > .01)
