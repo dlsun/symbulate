03170fed940adf0fe68a48b4df715abd4dc8b6aa
Dennis Sun <dsun09@calpoly.edu>
2018-09-07 03:19:51 -0700

    distinguish between tuple and Tuple

--- symbulate/probability_space.py
+++ symbulate/probability_space.py
@@ -45,7 +45,7 @@ class ProbabilitySpace:
                 return result
         else:
             def draw():
-                return join(*[self.draw() for _ in range(exponent)])
+                return tuple([self.draw() for _ in range(exponent)])
         return ProbabilitySpace(draw)
             
 
